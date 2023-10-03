d500521098b67d2da2be5ef779a0e86908d1ea29
Alexandros Christopoulos <alex1951132@gmail.com>
2023-10-03 13:15:18 +0300

    created example root service

--- src/controllers/root.ts
+++ src/controllers/root.ts
@@ -1,3 +1,10 @@
 import { Request, Response } from 'express';
+import { calculateMessage } from 'services/root';
 
-export const getRoot = (req: Request, res: Response) => res.sendStatus(200);
+export const getRoot = (req: Request, res: Response) => {
+  const { name } = req.query;
+
+  return res.status(200).json({
+    msg: calculateMessage(name?.toString()),
+  });
+};
---
+++ src/services/root.ts
@@ -0,0 +1,2 @@
+export const calculateMessage = (name?: string) =>
+  name ? `Hello ${name}` : `Hello world`;
